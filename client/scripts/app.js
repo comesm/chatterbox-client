0a14ec6c28ec6c885d147d759c2c44235ee8fecf
comesm <comesm@gmail.com>
2017-01-02 22:36:02 -0800

    Add room dropdown menu

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,30 @@
 var app = {
   init: function() {
+    window.rooms = {};
+    window.currentRoom = 'lobby';
     this.fetch();
     $('#chats').on('click', '.username', function(event) {
       app.handleUsernameClick(event);
     });
+
+    $('#send').on('submit', function(event) {
+      event.preventDefault();
+      // console.log($('#message'));
+      app.handleSubmit($('input:first').val());
+      console.log($('input:first').val());
+    });
+
+    $('.re-render').on('click', function(event) {
+      app.clearMessages();
+      app.fetch();
+    });
+
+    $('#roomSelect').on('click', function(event) {
+      window.currentRoom = event.target.text;
+      app.clearMessages();
+      app.fetch();
+    })
+    
   },
   server: 'https://api.parse.com/1/classes/messages',
   send: function(message) {
@@ -13,6 +34,9 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
+        //app.clearMessages();
+        app.fetch();
+        //app.renderAll(data);
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
@@ -24,11 +48,20 @@ var app = {
     $.ajax({
       url: this.server,
       type: 'GET',
+      data: {order: '-createdAt'},
       contentType: 'application/json',
       success: function (data) {
+    
         data.results.forEach(function(message) {
-          app.renderMessage(message);
+          if (!window.rooms[message.roomname] && !!message.roomname) {
+            window.rooms[message.roomname] = message.roomname;
+            app.renderRoom(message.roomname);
+          }
+          if (message.roomname === window.currentRoom) {
+            app.renderMessage(message);
+          }
         });
+        
         console.log('chatterbox: Message is received');
       },
       error: function (data) {
@@ -40,15 +73,26 @@ var app = {
     $('#chats').children().remove();
   },
   renderRoom: function(roomName) {
-    $('#roomSelect').append('<li>' + roomName + '</li>');
-  }, 
+
+      $('#roomSelect').append('<div><a href ="#">' + roomName + '</a></div>');
+    // }
+  },
+ 
   renderMessage: function(data) {
-    $('#chats').append($('<li class=message> <span class=username>' + data.username + ' ' + '</span>' + data.text + '</li>'));
+    // first check if each roomname matches any in the options array
+    //
+    var text = _.escape(data.text);
+    $('#chats').append($('<li class=message> <span class=username>' + data.username + ' ' + '</span>' + text + '</li>'));
   },
   handleUsernameClick: function(event) {
     console.log('Clicked username');
   },
   handleSubmit: function(message) {
-    console.log(message);
+    this.clearMessages();
+    this.send({
+      username: window.location.search.split('=')[1],
+      text: message,
+      roomname: window.currentRoom
+    });
   }
 };
